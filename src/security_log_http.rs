a1fc46c9544f50a89d99ceca6521ff01da52c6bc
Daniel Boline <ddboline@gmail.com>
2021-02-19 18:11:48 -0500

    actix-web -> warp

--- src/security_log_http.rs
+++ src/security_log_http.rs
@@ -1,54 +1,62 @@
-use actix_web::{
-    error::ResponseError,
-    web::{self, Data, Path, Query},
-    App, HttpResponse, HttpServer,
-};
 use anyhow::Error as AnyhowError;
+use std::convert::Infallible;
 use itertools::Itertools;
 use log::error;
 use serde::{Deserialize, Serialize};
 use stack_string::StackString;
 use std::env::var;
 use thiserror::Error;
+use warp::{Reply, reject::Reject, Rejection, Filter};
+use http::{StatusCode};
+use std::net::SocketAddr;
 
 use security_log_analysis_rust::{
     config::Config, pgpool_pg::PgPoolPg, reports::get_country_count_recent,
 };
 
-type HttpResult = Result<HttpResponse, ServiceError>;
+type WarpResult<T> = Result<T, Rejection>;
 
 #[derive(Error, Debug)]
 enum ServiceError {
-    #[error("Internal Server Error")]
-    InternalServerError,
     #[error("AnyhowError {0}")]
     AnyhowError(#[from] AnyhowError),
 }
 
-impl ResponseError for ServiceError {
-    fn error_response(&self) -> HttpResponse {
-        match self {
-            Self::InternalServerError => {
-                HttpResponse::InternalServerError().json("Internal Server Error, Please try later")
-            }
-            Self::AnyhowError(err) => {
-                error!("Received error {}", err);
-                HttpResponse::InternalServerError().json("Internal Server Error, Please try later")
-            }
-        }
-    }
+impl Reject for ServiceError {}
+
+#[derive(Serialize)]
+struct ErrorMessage {
+    code: u16,
+    message: String,
 }
 
-fn form_http_response(body: String) -> HttpResult {
-    Ok(HttpResponse::Ok()
-        .content_type("text/html; charset=utf-8")
-        .body(body))
-}
+pub async fn error_response(err: Rejection) -> Result<Box<dyn Reply>, Infallible> {
+    let code: StatusCode;
+    let message: &str;
 
-#[derive(Serialize, Deserialize)]
-struct ServiceLocation {
-    service: StackString,
-    location: StackString,
+    if err.is_not_found() {
+        code = StatusCode::NOT_FOUND;
+        message = "NOT FOUND";
+    } else if let Some(service_error) = err.find::<ServiceError>() {
+        error!("{:?}", service_error);
+        code = StatusCode::INTERNAL_SERVER_ERROR;
+        message = "Internal Server Error, Please try again later";
+    } else if err.find::<warp::reject::MethodNotAllowed>().is_some() {
+        code = StatusCode::METHOD_NOT_ALLOWED;
+        message = "METHOD NOT ALLOWED";
+    } else {
+        error!("Unknown error: {:?}", err);
+        code = StatusCode::INTERNAL_SERVER_ERROR;
+        message = "Internal Server Error, Please try again later";
+    };
+
+    let reply = warp::reply::json(&ErrorMessage {
+        code: code.as_u16(),
+        message: message.to_string(),
+    });
+    let reply = warp::reply::with_status(reply, code);
+
+    Ok(Box::new(reply))
 }
 
 #[derive(Serialize, Deserialize)]
@@ -57,22 +65,24 @@ struct AttemptsQuery {
 }
 
 async fn intrusion_attempts(
-    path: Path<ServiceLocation>,
-    query: Query<AttemptsQuery>,
-    data: Data<AppState>,
-) -> HttpResult {
+    service: StackString,
+    location: StackString,
+    query: AttemptsQuery,
+    data: AppState,
+) -> WarpResult<impl Reply> {
     let template = include_str!("../templates/COUNTRY_TEMPLATE.html");
-    let server = format!("{}.ddboline.net", path.location);
+    let server = format!("{}.ddboline.net", location);
     let ndays = query.ndays.unwrap_or(30);
-    let results = get_country_count_recent(&data.db, &path.service, &server, ndays)
-        .await?
+    let results = get_country_count_recent(&data.db, &service, &server, ndays)
+        .await.map_err(Into::<ServiceError>::into)?
         .into_iter()
         .map(|(x, y)| format!(r#"["{}", {}]"#, x, y))
         .join(",");
     let body = template.replace("PUTLISTOFCOUNTRIESANDATTEMPTSHERE", &results);
-    form_http_response(body)
+    Ok(warp::reply::html(body))
 }
 
+#[derive(Clone)]
 struct AppState {
     db: PgPoolPg,
 }
@@ -86,19 +96,30 @@ async fn start_app() -> Result<(), AnyhowError> {
         .and_then(|s| s.parse().ok())
         .unwrap_or(4086);
 
-    HttpServer::new(move || {
-        App::new().data(AppState { db: pool.clone() }).service(
-            web::resource("/security_log/intrusion_attempts/{service}/{location}")
-                .route(web::get().to(intrusion_attempts)),
-        )
-    })
-    .bind(&format!("127.0.0.1:{}", port))?
-    .run()
-    .await?;
+    let data = AppState { db: pool.clone() };
+    let data = warp::any().map(move || data.clone());
+
+    let intrusion_attemps_path = warp::path!("security_log" / "intrusion_attempts" / StackString / StackString)
+        .and(warp::path::end())
+        .and(warp::get())
+        .and(warp::query())
+        .and(data.clone())
+        .and_then(intrusion_attempts);
+
+        let cors = warp::cors()
+        .allow_methods(vec!["GET"])
+        .allow_header("content-type")
+        .allow_any_origin()
+        .build();
+
+    let routes = intrusion_attemps_path.recover(error_response).with(cors);
+    let addr: SocketAddr = format!("127.0.0.1:{}", port).parse()?;
+    warp::serve(routes).bind(addr).await;
+
     Ok(())
 }
 
-#[actix_web::main]
+#[tokio::main]
 async fn main() -> Result<(), AnyhowError> {
     start_app().await
 }
