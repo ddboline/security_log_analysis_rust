b14459e86c422836a86cff1282048d59a3aad3d4
Daniel Boline <ddboline@gmail.com>
2021-04-19 08:02:30 -0400

    clippy

--- src/lib.rs
+++ src/lib.rs
@@ -10,6 +10,8 @@
 #![allow(clippy::shadow_unrelated)]
 #![allow(clippy::missing_errors_doc)]
 #![allow(clippy::used_underscore_binding)]
+#![allow(clippy::upper_case_acronyms)]
+#![allow(clippy::missing_errors_doc)]
 
 #[macro_use]
 extern crate diesel;
