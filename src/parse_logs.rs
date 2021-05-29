066724a2bb31017815b708c7e4b0d2b31130dee2
Daniel Boline <ddboline@gmail.com>
2021-05-28 21:04:06 -0400

    insert -> insert_single

--- src/parse_logs.rs
+++ src/parse_logs.rs
@@ -278,7 +278,7 @@ async fn process_systemd_sshd_output(
                 };
                 let pool = pool.clone();
                 debug!("proc sshd {:?}", log_entry);
-                spawn_blocking(move || IntrusionLogInsert::insert(&pool, &[log_entry])).await??;
+                spawn_blocking(move || IntrusionLogInsert::insert_single(&pool, &log_entry)).await??;
             } else if line.contains("_SOURCE_REALTIME_TIMESTAMP") && line.contains("nginx") {
                 let log: ServiceLogLine = serde_json::from_str(&line)?;
                 if let Some(log_line) = log.parse_nginx()? {
@@ -291,7 +291,7 @@ async fn process_systemd_sshd_output(
                     };
                     let pool = pool.clone();
                     debug!("proc nginx {:?}", log_entry);
-                    spawn_blocking(move || IntrusionLogInsert::insert(&pool, &[log_entry]))
+                    spawn_blocking(move || IntrusionLogInsert::insert_single(&pool, &log_entry))
                         .await??;
                 }
             }
