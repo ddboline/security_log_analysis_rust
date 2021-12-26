5ba48bcc8f56b928eb900b5fcbd3a3d1d03cc8af
Daniel Boline <ddboline@gmail.com>
2021-12-26 08:36:26 -0500

    delete polars_main

--- src/polars_main.rs
+++
@@ -1,9 +0,0 @@
-use anyhow::Error;
-
-use security_log_analysis_rust::polars_analysis::AnalysisOpts;
-
-#[tokio::main]
-async fn main() -> Result<(), Error> {
-    env_logger::init();
-    AnalysisOpts::parse_opts().await
-}
