e461b8b6663c53b97fb4149fd0a106d39555f201
Daniel Boline <ddboline@gmail.com>
2021-12-26 08:33:54 -0500

    move shared types into lib

--- src/lib.rs
+++ src/lib.rs
@@ -26,13 +26,16 @@ pub mod reports;
 pub mod s3_sync;
 
 use anyhow::{format_err, Error};
+use chrono::{DateTime, Utc};
 use log::error;
+use postgres_query::FromSqlRow;
 use rand::{
     distributions::{Alphanumeric, Distribution, Uniform},
     thread_rng,
 };
+use rweb::Schema;
 use stack_string::StackString;
-use std::{future::Future, path::Path, time::Duration};
+use std::{fmt, future::Future, path::Path, str::FromStr, time::Duration};
 use tokio::{process::Command, time::sleep};
 
 pub async fn exponential_retry<T, U, F>(closure: T) -> Result<U, Error>
@@ -75,4 +78,99 @@ pub async fn get_md5sum(filename: &Path) -> Result<StackString, Error> {
         }
     }
     Err(format_err!("Command failed"))
+}
+
+#[derive(FromSqlRow)]
+pub struct CountryCount {
+    pub country: StackString,
+    pub count: i64,
+}
+
+#[derive(Debug, Clone, Copy, Schema)]
+pub enum Host {
+    Home,
+    Cloud,
+}
+
+impl FromStr for Host {
+    type Err = Error;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        match s {
+            "home" | "home.ddboline.net" => Ok(Self::Home),
+            "cloud" | "cloud.ddboline.net" => Ok(Self::Cloud),
+            _ => Err(format_err!("Not a valid Host")),
+        }
+    }
+}
+
+impl Host {
+    pub fn get_prefix(self) -> &'static str {
+        match self {
+            Self::Home => "home",
+            Self::Cloud => "cloud",
+        }
+    }
+
+    pub fn to_str(self) -> &'static str {
+        match self {
+            Self::Home => "home.ddboline.net",
+            Self::Cloud => "cloud.ddboline.net",
+        }
+    }
+}
+
+impl fmt::Display for Host {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{}", self.to_str())
+    }
+}
+
+#[derive(Debug, Clone, Copy, Schema)]
+pub enum Service {
+    Apache,
+    Nginx,
+    Ssh,
+}
+
+impl FromStr for Service {
+    type Err = Error;
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        match s {
+            "apache" => Ok(Self::Apache),
+            "nginx" => Ok(Self::Nginx),
+            "ssh" => Ok(Self::Ssh),
+            _ => Err(format_err!("Not a valid service")),
+        }
+    }
+}
+
+impl Service {
+    pub fn to_str(self) -> &'static str {
+        match self {
+            Self::Apache => "apache",
+            Self::Nginx => "nginx",
+            Self::Ssh => "ssh",
+        }
+    }
+}
+
+impl fmt::Display for Service {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{}", self.to_str())
+    }
+}
+
+#[derive(Debug)]
+pub struct DateTimeInput(pub DateTime<Utc>);
+
+impl FromStr for DateTimeInput {
+    type Err = Error;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        DateTime::parse_from_rfc3339(s)
+            .map(|d| d.with_timezone(&Utc))
+            .map(Self)
+            .map_err(Into::into)
+    }
 }
