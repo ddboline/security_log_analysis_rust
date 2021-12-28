373cf3d7ce636cc5fb4ba8fe940386aa65ee7b5a
Daniel Boline <ddboline@gmail.com>
2021-12-27 19:15:29 -0500

    version bump

--- src/host_country_metadata.rs
+++ src/host_country_metadata.rs
@@ -129,7 +129,7 @@ impl HostCountryMetadata {
             })
             .ok_or_else(|| format_err!("Failed to extract IP address from {}", host))?;
         let url = Url::parse("https://ipwhois.app/json/")?.join(&ipaddr)?;
-        println!("{}", url);
+        debug!("{}", url);
         let resp = self.client.get(url).send().await?.error_for_status()?;
         let output: IpWhoIsOutput = resp.json().await?;
         Ok(output.country_code)
--- src/parse_opts.rs
+++ src/parse_opts.rs
@@ -193,11 +193,14 @@ impl ParseOpts {
                 let new_hosts: HashSet<_> =
                     inserts.iter().map(|item| item.host.to_string()).collect();
 
-                let futures = new_hosts.into_iter().map(|host| {
-                    let metadata = metadata.clone();
-                    async move { metadata.get_country_info(&host).await }
-                });
-                try_join_all(futures).await?;
+                for host in new_hosts {
+                    metadata.get_country_info(&host).await?;
+                }
+                // let futures = new_hosts.into_iter().map(|host| {
+                //     let metadata = metadata.clone();
+                //     async move { metadata.get_country_info(&host).await }
+                // });
+                // try_join_all(futures).await?;
 
                 let mut existing_entries = Vec::new();
 
