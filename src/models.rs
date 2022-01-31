d0cb7eee904fdada634e8b2a3ae24594c1cc01e9
Daniel Boline <ddboline@gmail.com>
2022-01-30 19:37:46 -0500

    rustfmt

--- src/models.rs
+++ src/models.rs
@@ -132,16 +132,18 @@ impl HostCountry {
 
     pub async fn get_dangling_hosts(pool: &PgPool) -> Result<Vec<StackString>, Error> {
         #[derive(FromSqlRow)]
-        struct Wrapper{
+        struct Wrapper {
             host: StackString,
         }
 
-        let query = query!(r#"
-            SELECT distinct a.host
-            FROM intrusion_log a
-            LEFT JOIN host_country b ON a.host = b.host
-            WHERE b.host IS NULL
-        "#);
+        let query = query!(
+            r#"
+                SELECT distinct a.host
+                FROM intrusion_log a
+                LEFT JOIN host_country b ON a.host = b.host
+                WHERE b.host IS NULL
+            "#
+        );
         let conn = pool.get().await?;
         let rows: Vec<Wrapper> = query.fetch(&conn).await?;
         Ok(rows.into_iter().map(|x| x.host).collect())
